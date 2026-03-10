60c405e3a649d6df27dfa8ad26d31f322de76cc1
Bening Putri Nareswari Sukarno <bening14mei@gmail.com>
2026-03-10 09:03:52 +0700

    Add files via upload

---
+++ Modul01/3 2B.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var kiri, kanan float64
+	var selisih float64
+
+	for {
+		fmt.Print("Masukan berat belanjaan di kedua kantong: ")
+		fmt.Scan(&kiri, &kanan)
+
+		if kiri < 0 || kanan < 0 || kiri+kanan > 150 {
+			break
+		}
+
+		if kiri > kanan {
+			selisih = kiri - kanan
+		} else {
+			selisih = kanan - kiri
+		}
+
+		if selisih >= 9 {
+			fmt.Println("Sepeda motor pak Andi akan oleng: true")
+		} else {
+			fmt.Println("Sepeda motor pak Andi akan oleng: false")
+		}
+	}
+
+	fmt.Println("Proses selesai.")
+}
